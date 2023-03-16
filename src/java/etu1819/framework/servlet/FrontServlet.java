36c3b3378fd7570f78333d235374babc17f2dc8d
Mickael-1819 <mickael.gaiden@gmail.com>
2023-03-17 00:49:04 +0300

    Sprint1

---
+++ src/java/etu1819/framework/servlet/FrontServlet.java
@@ -0,0 +1,17 @@
+package etu1819.framework.servlet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author gaiden
+ */
+
+public class FrontServlet {
+
+ 
+  }
+
+

79787ae56446c480635ef7ac808437d44a429d2b
Mickael-1819 <119859344+Mickael-1819@users.noreply.github.com>
2023-07-07 19:54:45 +0300

    Sprint1

--- testFramework2/build/web/WEB-INF/classes/deployWar.sh
+++
@@ -1,4 +0,0 @@
-rm -r ../testFramework.war
-jar -cvf ../testFramework.war * 
-rm -r /home/yoan/apache-tomcat-10.0.27/webapps/testFramework.war
-cp -r ../testFramework.war /home/yoan/apache-tomcat-10.0.27/webapps/testFramework.war
